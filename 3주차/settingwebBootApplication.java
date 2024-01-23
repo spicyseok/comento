ee9df3508f244299c13c1da05ce9869bd9b21620
spicyseok <117507997+spicyseok@users.noreply.github.com>
2024-01-23 16:18:54 +0900

    Update settingwebBootApplication.java

--- 3주차/settingwebBootApplication.java
+++ 3주차/settingwebBootApplication.java
@@ -1,1 +1,14 @@
+package com.devfun.settingweb_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootSettingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
 
