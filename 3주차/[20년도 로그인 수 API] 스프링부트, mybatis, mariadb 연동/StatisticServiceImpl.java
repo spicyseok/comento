58652c8ff283f7abe3b174cfdac6d57a6dbc12d7
spicyseok <117507997+spicyseok@users.noreply.github.com>
2024-01-24 15:49:06 +0900

    Create StatisticServiceImpl.java

---
+++ 3주차/[20년도 로그인 수 API] 스프링부트, mybatis, mariadb 연동/StatisticServiceImpl.java
@@ -0,0 +1,36 @@
+package com.devfun.settingweb_boot.service;
+ 
+ 
+import java.util.HashMap;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+ 
+import com.devfun.settingweb_boot.dao.StatisticMapper;
+ 
+@Service
+public class StatisticServiceImpl implements StatisticService {
+    
+    
+    @Autowired
+    private StatisticMapper uMapper;
+    
+    @Override
+    public HashMap<String, Object> yearloginNum (String year) {
+        // TODO Auto-generated method stub
+        HashMap<String, Object> retVal = new HashMap<String,Object>();
+        
+        try {
+            retVal = uMapper.selectYearLogin(year);
+            retVal.put("year", year);
+            retVal.put("is_success", true);
+            
+        }catch(Exception e) {
+            retVal.put("totCnt", -999);
+            retVal.put("year", year);
+            retVal.put("is_success", false);
+        }
+        
+        return retVal;
+    }
+ 
+}
